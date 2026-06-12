8d5e03957246e270626f9e1a45f9e0c058cf111d
Valentin Zaytsev <v.zaytsev@kefirgames.com>
2026-06-12 09:40:14 +0300

    extend GPUBlendState with predefined blend modes

--- Istok.WebGPU/Structs/GPUBlendState.cs
+++ Istok.WebGPU/Structs/GPUBlendState.cs
@@ -11,4 +11,68 @@ public struct GPUBlendState()
 			color = value.Color,
 			alpha = value.Alpha,
 		};
+
+	public static GPUBlendState Opaque => new GPUBlendState()
+	{
+		Color = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.One,
+			DstFactor = WGPUBlendFactor.Zero
+		},
+		Alpha = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.One,
+			DstFactor = WGPUBlendFactor.Zero
+		}
+	};
+
+	public static GPUBlendState Transparency => new GPUBlendState()
+	{
+		Color = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.SrcAlpha,
+			DstFactor = WGPUBlendFactor.OneMinusSrcAlpha
+		},
+		Alpha = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.One,
+			DstFactor = WGPUBlendFactor.OneMinusSrcAlpha
+		}
+	};
+
+	public static GPUBlendState PremultipliedAlpha => new GPUBlendState()
+	{
+		Color = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.One,
+			DstFactor = WGPUBlendFactor.OneMinusSrcAlpha
+		},
+		Alpha = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.One,
+			DstFactor = WGPUBlendFactor.OneMinusSrcAlpha
+		}
+	};
+
+	public static GPUBlendState Additive => new GPUBlendState()
+	{
+		Color = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.SrcAlpha,
+			DstFactor = WGPUBlendFactor.One
+		},
+		Alpha = new GPUBlendComponent
+		{
+			Operation = WGPUBlendOperation.Add,
+			SrcFactor = WGPUBlendFactor.Zero,
+			DstFactor = WGPUBlendFactor.One
+		}
+	};
 }
